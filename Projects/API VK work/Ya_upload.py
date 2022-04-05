7c487b519780561cd48539040744c84938ee18e8
MukhinAnatol <anatoliymukhin30@gmail.com>
2022-04-05 20:29:02 +1000

    file updated

--- Projects/API VK work/Ya_upload.py
+++ Projects/API VK work/Ya_upload.py
@@ -18,16 +18,17 @@ class YaUploader:
         headers = self.get_headers()
         create_folder_url = 'https://cloud-api.yandex.net/v1/disk/resources'
         response = requests.put(create_folder_url, headers=headers, params=params)
+        print(response.json())
         if response.status_code == 201:
             print(f'Папка {folder_path} успешно создана')
-            return response.json()
 
-    # def upload(self, file_path: str, file_name: str):
-    #     href = self.get_url(file_path=file_path).get('href')
-    #     upload = requests.put(href, data=open(file_name, 'rb'))
-    #     upload.raise_for_status()
-    #     if upload.status_code == 201:
-    #         print("Success")
+    def upload(self, file_path, url: str):
+        params = {
+            'path': file_path,
+            'url' : url
+        }
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        response = requests.post(url=upload_url, headers= self.get_headers(), params=params)
 
 Ya = YaUploader(Ya_Token)
-Ya.create_folder('new_folder')
+Ya.upload('test_path', 'https://sun9-48.userapi.com/impf/hZBImqkCXbzy3VGIhn30Jj4P3RWurerkaZZSnw/oDCgWwpSe34.jpg?size=580x807&quality=96&sign=e2d576d1b2cf05e8e8b9d9be2b4b2fb8&c_uniq_tag=UzBC-6GSTe_a1WxqGKsHwPyCUsS0NgHsvI_yuaG5BkQ&type=album')
